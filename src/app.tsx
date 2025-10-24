0f7a815240e783adedd300fa4441741e88ab213e
Felipe DR <felipe.corp7@gmail.com>
2025-10-24 19:37:50 -0300

    feat(app): add ThemeProvider and GlobalStyle

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes'
+
 export function App() {
-  return <div>Coffee Delivery</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
