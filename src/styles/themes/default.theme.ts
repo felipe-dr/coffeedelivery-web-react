6c9bdbae6aa964bf59805d91d68b96c4debffba9
Felipe DR <felipe.corp7@gmail.com>
2025-10-24 19:33:55 -0300

    feat(styles): add default theme

---
+++ src/styles/themes/default.theme.ts
@@ -0,0 +1,43 @@
+export const defaultTheme = {
+  colors: {
+    yellowLigh: '#F1E9C9',
+    yellow: '#DBAC2C',
+    yellowDark: '#C47F17',
+
+    purpleLight: '#EBE5F9',
+    purple: '#8047F8',
+    purpleDark: '#4B2995',
+
+    white: '#FFFFFF',
+    background: '#FAFAFA',
+    baseCard: '#F3F2F2',
+    baseInput: '#EDEDED',
+    baseButton: '#E6E5E5',
+    baseHover: '#D7D5D5',
+    baseLabel: '#8D8686',
+    baseText: '#574F4D',
+    baseSubtitle: '#403937',
+    baseTitle: '#272221',
+  },
+  title: {
+    xl: "800 3rem/130% 'Baloo 2', sans-serif",
+    l: "800 2rem/130% 'Baloo 2', sans-serif",
+    m: "800 1.5rem/130% 'Baloo 2', sans-serif",
+    s: "600 1.25rem/130% 'Baloo 2', sans-serif",
+    xs: "600 1.125rem/130% 'Baloo 2', sans-serif",
+  },
+  text: {
+    l: "600 1.25rem/130% 'Roboto', sans-serif",
+    m: "600 1rem/130% 'Roboto', sans-serif",
+    s: "400 0.875rem/130% 'Roboto', sans-serif",
+    xs: "600 0.75rem/130% 'Roboto', sans-serif",
+
+    tag: "600 0.0.625rem/130% 'Roboto', sans-serif",
+    buttonG: "600 0.875rem/160% 'Roboto', sans-serif",
+    buttonM: "400 0.75rem/160% 'Roboto', sans-serif",
+  },
+  grid: {
+    container: '72.5rem',
+    gutter: '1.25rem',
+  },
+}
