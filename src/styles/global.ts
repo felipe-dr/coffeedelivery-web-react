57f39b7dff7abf238ce8563503970c56dcb731f0
Felipe DR <felipe.corp7@gmail.com>
2025-10-27 20:52:18 -0300

    style(styles): add base styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,6 +8,22 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    background: ${(props) => props.theme.colors.white};
+    color: ${(props) => props.theme.colors.baseText};
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  :focus {
+    outline: none;
   }
 `
