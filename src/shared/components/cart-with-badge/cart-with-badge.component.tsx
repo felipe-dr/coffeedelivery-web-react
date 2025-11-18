484a72545171658163b0c086acf76bd7bb9572c8
Felipe DR <felipe.corp7@gmail.com>
2025-11-17 22:25:29 -0300

    feat(shared): add value from context

--- src/shared/components/cart-with-badge/cart-with-badge.component.tsx
+++ src/shared/components/cart-with-badge/cart-with-badge.component.tsx
@@ -1,12 +1,16 @@
 import { ShoppingCart } from 'phosphor-react'
+import { CartContext } from '@/contexts'
+import { useContext } from 'react'
 
 import { CartWithBadge, CartWithBadgeTotal } from './cart-with-badge.styles'
 
 export function CartWithBadgeComponent() {
+  const { cart } = useContext(CartContext)
+
   return (
     <CartWithBadge>
       <ShoppingCart weight="fill" />
-      <CartWithBadgeTotal>0</CartWithBadgeTotal>
+      <CartWithBadgeTotal>{cart.length}</CartWithBadgeTotal>
     </CartWithBadge>
   )
 }
