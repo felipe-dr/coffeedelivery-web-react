1d45d8cfb4a32cdf241ce449e8c9e471a15e7208
Felipe DR <felipe.corp7@gmail.com>
2025-11-17 22:31:36 -0300

    feat(app): add CartContextProvider

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { CartContextProvider } from './contexts'
 
 import { Router } from './routes/router'
 import { GlobalStyle } from './styles/global'
@@ -9,7 +10,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CartContextProvider>
+          <Router />
+        </CartContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
