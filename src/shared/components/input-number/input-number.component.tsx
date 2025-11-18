6b686bd1ea57b9f85f29859d8c7242ff10bfb25c
Felipe DR <felipe.corp7@gmail.com>
2025-11-17 22:31:09 -0300

    feat(shared): add integration with context

--- src/shared/components/input-number/input-number.component.tsx
+++ src/shared/components/input-number/input-number.component.tsx
@@ -1,5 +1,6 @@
+import { CartContext } from '@/contexts'
 import { Minus, Plus } from 'phosphor-react'
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, useContext } from 'react'
 
 import {
   InputNumberBaseButton,
@@ -7,16 +8,32 @@ import {
 } from './input-number.styles'
 
 interface InputNumberComponentProps
-  extends InputHTMLAttributes<HTMLInputElement> {}
+  extends InputHTMLAttributes<HTMLInputElement> {
+  itemId: string
+  value: number
+}
 
-export function InputNumberComponent() {
+export function InputNumberComponent({
+  itemId,
+  value,
+}: InputNumberComponentProps) {
+  const { increaseItemQuantity, decreaseItemQuantity } = useContext(CartContext)
+
+  const handleIncreaseItemQuantity = () => {
+    increaseItemQuantity(itemId)
+  }
+
+  const handleDecreaseItemQuantity = () => {
+    decreaseItemQuantity(itemId)
+  }
+
   return (
     <InputNumberWrapper>
-      <InputNumberBaseButton>
+      <InputNumberBaseButton onClick={handleDecreaseItemQuantity}>
         <Minus size="14" />
       </InputNumberBaseButton>
-      1
-      <InputNumberBaseButton>
+      {value || 0}
+      <InputNumberBaseButton onClick={handleIncreaseItemQuantity}>
         <Plus size="14" />
       </InputNumberBaseButton>
     </InputNumberWrapper>
